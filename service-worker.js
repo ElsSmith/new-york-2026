218d20fd176349f4e5d8e06e8472cf013efdeed8
ElsSmith <info@alpenvilla.com>
2026-09-21 18:04:34 +0200

    Update cache naar v2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ny2026-v1';
+const CACHE = 'ny2026-v2';
 const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon.svg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
